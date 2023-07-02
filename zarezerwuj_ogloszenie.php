b63a5371a85fccb534ce6d0f4e7bc2a8f9bb7580
alicebuk <136857971+alicebuk@users.noreply.github.com>
2023-07-02 13:52:04 +0200

    Update zarezerwuj_ogloszenie.php

--- zarezerwuj_ogloszenie.php
+++ zarezerwuj_ogloszenie.php
@@ -19,15 +19,38 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST["zarezerwuj"])) {
         $email_rezerwacja = $_POST['email_rezerwacja'];
         $id_ogloszenia = $_POST['id_ogloszenia'];
+        
+        // Sprawdzenie, czy ogłoszenie jest już zarezerwowane
+        $sql_check = "SELECT zarezerwowane FROM ogloszenia WHERE id = '$id_ogloszenia'";
+        $result_check = $conn->query($sql_check);
+        
+        if ($result_check->num_rows > 0) {
+            $row_check = $result_check->fetch_assoc();
+            $zarezerwowane = $row_check['zarezerwowane'];
+        
+            if ($zarezerwowane == 1) {
+                echo "<script>alert('To ogłoszenie jest już zarezerwowane.')</script>";
+                header("Refresh:0.2; url=index.php");
+                exit();
+            }
+            else{
+                // Aktualizacja rekordu w bazie danych
+                $sql = "UPDATE ogloszenia SET zarezerwowane = 1, mail_z = '$email_rezerwacja' WHERE id = '$id_ogloszenia'";
+            }
+        }
+        
+        if ($conn->query($sql) === TRUE) {
+            echo "<script>alert('Ogłoszenie zarezerwowane')</script>";
+            header("Refresh:0.2; url=index.php"); 
+        } else {
+            echo "Błąd: " . $sql . "<br>" . $conn->error;
+        }
     }
-    // Aktualizacja rekordu w bazie danych
-    $sql = "UPDATE ogloszenia SET zarezerwowane = 1, mail_z = '$email_rezerwacja' WHERE id = '$id_ogloszenia'";
     
-    if ($conn->query($sql) === TRUE) {
-        echo "Ogłoszenie zarezerwowane.";
-    } else {
-        echo "Błąd: " . $sql . "<br>" . $conn->error;
-    }
+    $conn->close();
+}
+?>
+
     
     $conn->close();
 }
